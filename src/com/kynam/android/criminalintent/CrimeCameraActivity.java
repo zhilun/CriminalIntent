1e10e49c88eb5f9157c7b11388c136c61ca561f5
zhilun <trankynam2612@gmail.com>
2014-12-19 08:56:58 +0700

    End of Chapter 19 Camera I: Viewfinder Page 319

--- src/com/kynam/android/criminalintent/CrimeCameraActivity.java
+++ src/com/kynam/android/criminalintent/CrimeCameraActivity.java
@@ -1,8 +1,20 @@
 package com.kynam.android.criminalintent;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class CrimeCameraActivity extends SingleFragmentActivity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		// Hide the window title.
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// Hide the status bar and other OS-level chrome
+		getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		super.onCreate(savedInstanceState);
+	}
 
 	@Override
 	protected Fragment createFragment() {
